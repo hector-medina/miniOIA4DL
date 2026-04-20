304480969157607d700f57e19c601874e8f074e5
hector-medina <hector.medina.cabanelas@gmail.com>
2026-04-20 18:29:51 +0200

    add dense optimization

--- modules/utils.py
+++ modules/utils.py
@@ -1,11 +1,16 @@
 #PISTA: es esta la mejor forma de hacer una matmul?
+"""
 def matmul_biasses(A, B, C, bias):
     m, p, n = A.shape[0], A.shape[1], B.shape[1]
     
     for i in range(m):
-        for j in range(n):
-            for k in range(p):
+        for k in range(p):
+            for j in range(n):
                 C[i][j] += A[i][k] * B[k][j]
-            C[i][j] += bias[j]
+            #C[i][j] += bias[j]
+    C+= bias
     return C
-
+"""
+def matmul_biasses(A,B,C,bias):
+    C = A @ B + bias
+    return C
